08fe90406f35950c509dafe3367b736973b9e014
ruchika19021995 <ruchikaverma69@gmail.com>
2020-06-09 20:52:08 +0530

    123 print

--- CAMutipleTargets/ViewController.swift
+++ CAMutipleTargets/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         Crashlytics.crashlytics()
         super.viewDidLoad()
+        
+        print("123")
         // Do any additional setup after loading the view.
     }
 
